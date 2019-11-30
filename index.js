caf6b96d4f076285d4508ac5a171cdc1a290edc3
pponce <pponce@users.noreply.github.com>
2019-11-29 20:38:05 -0800

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ mPowerAccessory.prototype.setState = function(state, callback) {
   state = (state == true || state == 1) ? 1 : 0;
 
   //use SSH to powerstrip to send on off commands directly to powerstrip
-  var cmdUpdate = 'echo $(ssh ubnt@' + this.url + ' "echo ' + state + ' > /proc/power/relay' + this.id + ';cd /proc/power;grep \'\' relay' + this.id + '*;exit;")';
+  var cmdUpdate = 'echo $(ssh -oKexAlgorithms=+diffie-hellman-group1-sha1 ubnt@' + this.url + ' "echo ' + state + ' > /proc/power/relay' + this.id + ';cd /proc/power;grep \'\' relay' + this.id + '*;exit;")';
   //this.log("state variable = " + state + ".");
   var stateName = (state == 1) ? 'on' : 'off';
 
@@ -100,7 +100,7 @@ mPowerAccessory.prototype.setState = function(state, callback) {
 mPowerAccessory.prototype.getState = function(callback) {
   var exec = require('child_process').exec;
 	
-  var cmdStatus = 'echo $(ssh ubnt@' + this.url + ' "cd /proc/power;grep \'\' relay' + this.id + '*;exit;")';
+  var cmdStatus = 'echo $(ssh -oKexAlgorithms=+diffie-hellman-group1-sha1 ubnt@' + this.url + ' "cd /proc/power;grep \'\' relay' + this.id + '*;exit;")';
 
       exec(cmdStatus, function(error, stdout, stderr) {
         if (!error) {
@@ -127,7 +127,7 @@ mPowerAccessory.prototype.getState = function(callback) {
 mPowerAccessory.prototype.getOutletInUse = function(callback) {
   var exec = require('child_process').exec;
 
-  var cmdOutletInUseStatus = 'echo $(ssh ubnt@' + this.url + ' "cd /proc/power;grep \'\' active_pwr' + this.id + '*;exit;")';
+  var cmdOutletInUseStatus = 'echo $(ssh -oKexAlgorithms=+diffie-hellman-group1-sha1 ubnt@' + this.url + ' "cd /proc/power;grep \'\' active_pwr' + this.id + '*;exit;")';
       
 	  exec(cmdOutletInUseStatus, function(error, stdout, stderr) {
         if (!error) {
